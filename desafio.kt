077b2785d35589257dc5bc8feb3b9214fb289297
Marcus Vinicius <87397063+ItsAMeMarcus@users.noreply.github.com>
2023-09-19 18:46:52 -0300

    Exercicio Terminado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,53 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val id: Long, var nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
+    private val inscritos = mutableListOf<Usuario>()
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        val estaInscrito = inscritos.filter{it.id == (usuario.id)}
+
+        if (estaInscrito.isNotEmpty()){
+            println("Esse aluno já está cadastrado")
+        }
+        else{
+            inscritos.add(usuario)
+
+            println("Aluno cadastrado com sucesso")
+        }
+    }
+
+    fun matricularListaAlunos(vararg novosInscritos:Usuario){
+
+        val inscritosValidos = novosInscritos.filter{it.id !=(inscritos.iterator().next().id)}
+
+        inscritos.addAll(inscritosValidos)
+
+        println("Lista de novos alunos cadastrados: ")
+
+        inscritosValidos.forEach { usuario -> println("${usuario.nome}")}
+
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val exemplo = Formacao(
+        "Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin", 
+        listOf<ConteudoEducacional>(ConteudoEducacional("Formações Educacionais da DIO")), 
+        Nivel.BASICO
+    )
+
+    exemplo.matricular(Usuario(1L,"Maria"))
+
+    exemplo.matricular(Usuario(1L,"Maria"))
+
+    exemplo.matricularListaAlunos(
+        Usuario(1L,"Maria"),
+        Usuario(2L,"João"), 
+        Usuario(3L,"José")
+    )
 }
